eae39691c902617d75a458a165ed255ff1bc1812
RbYy <robert.brumat@gmail.com>
2016-02-29 22:25:49 +0100

    added new model List

--- hello/tests.py
+++ hello/tests.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from hello.views import index
 from django.template.loader import render_to_string
-from hello.models import Item
+from hello.models import Item, List
 
 # Create your tests here.
 
@@ -20,16 +20,23 @@ class HelloTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -38,15 +45,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,
                          'The first (ever) list item')
-        self.assertEqual(second_saved_item.text,
-                         'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List()
+        list_.save()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -61,6 +71,7 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
+        
         self.client.post('/lists/new/',
                          data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Greeting, Item
+from .models import Item, List
 
 
 # Create your views here.
@@ -15,17 +15,9 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
-
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List()
+    list_.save()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 hello = None
