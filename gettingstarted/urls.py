4a044c4cc17ef27fdb2957102535eaf29de7bf74
RbYy <robert.brumat@gmail.com>
2016-03-01 19:05:39 +0100

    new URL + view for adding to existing lists. FT passes :-)

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,7 +11,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^lists/the-only-list-in-the-world/$', hello.views.view_list, name='view_list'),
     url(r'^lists/new/$', hello.views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', hello.views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', hello.views.add_item, name='add_item'),
     url(r'^admin/', include(admin.site.urls)),
 ]
