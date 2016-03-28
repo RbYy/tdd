c0169be297ce25a6278a4fea4caeae8b41169d24
RbYy <robert.brumat@gmail.com>
2016-03-28 17:09:15 +0200

    Split out unit tests into two files

--- hello/tests/test_views.py
+++ hello/tests/test_views.py
@@ -20,38 +20,7 @@ class HelloTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text,
-                         'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(TestCase):
-
     def test_displays_all_items(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
@@ -80,7 +49,6 @@ class ListViewTest(TestCase):
 
 
 class NewListTest(TestCase):
-
     def test_saving_a_POST_request(self):
 
         self.client.post('/lists/new/',
@@ -100,7 +68,6 @@ class NewListTest(TestCase):
 
 
 class NewItemTest(TestCase):
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -114,7 +81,6 @@ class NewItemTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
 
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
