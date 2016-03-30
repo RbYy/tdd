bd0e571cc7adafbd2ab9f4c2927334ff2057e03e
RbYy <robert.brumat@gmail.com>
2016-03-30 19:54:19 +0200

    rename all item input ids and names.

--- hello/views.py
+++ hello/views.py
@@ -18,7 +18,7 @@ def view_list(request, list_id):
     error = None
     if request.method == "POST":
         try:
-            item = Item(text=request.POST['item_text'],
+            item = Item(text=request.POST['text'],
                         list=list_)
             item.full_clean()
             item.save()
@@ -28,17 +28,20 @@ def view_list(request, list_id):
     return render(request,
                   'list.html',
                   {'list': list_,
-                   'error': error})
+                   'error': error,
+                   'form': ItemForm()
+                   })
 
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
     except ValidationError:
         list_.delete()
         error = "You can't have an empty list item"
-        return render(request, 'index.html', {"error": error})
+        return render(request, 'index.html', {"error": error,
+                                              'form': ItemForm()})
     return redirect(list_)
